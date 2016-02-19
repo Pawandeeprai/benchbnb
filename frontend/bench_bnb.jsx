422a9d7c64fdd09263724c7290a5976e8b6a0a70
pawandeeprai <pawandeeprai@gmail.com>
2016-02-19 15:30:41 -0800

    t quit

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(
+    <div>
+      Div you know that?
+    </div>,
+    document.getElementById('content')
+  );
+});
