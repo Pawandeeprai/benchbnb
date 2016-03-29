1a5cfbcebd9d8be7475a57ee5c356bea240626f3
pawandeeprai <pawandeeprai@gmail.com>
2016-03-29 13:54:19 -0700

    refactoring

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,5 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var root = document.getElementById('content');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
 
 var ApiUtil = require('./util/apiutil');
 var BenchStore = require('./stores/bench');
@@ -8,9 +13,26 @@ var ApiActions = require('./actions/api_actions');
 var Index = require('./components/index');
 var Search = require('./components/search');
 
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <header><h1>Bench BnB</h1></header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Search}/>
+  </Route>
+);
+
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(
-    <Search />,
+    <Route>{routes}</Route>,
     document.getElementById('content')
   );
 });
